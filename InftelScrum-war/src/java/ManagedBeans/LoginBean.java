c97354cc0970c55c68bb99503b2005efb3d3dbd4
inftel20 <inftel20@192.168.183.24>
2016-03-02 19:32:14 +0100

    UsuarioScrum actualizado, LoginBean con set/get, signInOut.js creado y fuera de la vista

--- InftelScrum-war/src/java/ManagedBeans/LoginBean.java
+++ InftelScrum-war/src/java/ManagedBeans/LoginBean.java
@@ -5,8 +5,12 @@
  */
 package ManagedBeans;
 
+import ejb.UsuarioScrumFacade;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import model.UsuarioScrum;
 
 /**
  *
@@ -15,11 +19,49 @@ import javax.faces.bean.RequestScoped;
 @ManagedBean
 @RequestScoped
 public class LoginBean {
+    @EJB
+    private UsuarioScrumFacade usuarioScrumFacade;
 
+    protected boolean logueado;
+    protected UsuarioScrum user;
+    protected String image;
+    
+    
+    
+    
     /**
      * Creates a new instance of LoginBean
      */
     public LoginBean() {
+    }
+    
+    @PostConstruct
+    public void init(){
+        this.user = usuarioScrumFacade.find(1);
+    }
+
+    public boolean isLogueado() {
+        return logueado;
+    }
+
+    public void setLogueado(boolean logueado) {
+        this.logueado = logueado;
+    }
+
+    public UsuarioScrum getUser() {
+        return user;
+    }
+
+    public void setUser(UsuarioScrum user) {
+        this.user = user;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
     }
     
 }
